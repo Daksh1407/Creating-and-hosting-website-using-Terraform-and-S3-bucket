de40e1aff35de7b51a922ac132715d8810351988
piyush1407 <102173748+Daksh1407@users.noreply.github.com>
2024-06-09 23:57:42 +0530

    Create script.js

---
+++ terraform_project/script.js
@@ -0,0 +1,95 @@
+    function cathome(){
+		$.ajax({
+			url	:	"homeaction.php",
+			method:	"POST",
+			data	:	{categoryhome:1},
+			success	:	function(data){
+				$("#get_category_home").html(data);
+				
+			}
+		})
+	}
+	//brand() is a funtion fetching brand record from database whenever page is load
+	function brand(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{brand:1},
+			success	:	function(data){
+				$("#get_brand").html(data);
+			}
+		})
+	}
+	//product() is a funtion fetching product record from database whenever page is load
+		function product(){
+		$.ajax({
+			url	:	"action.php",
+			method:	"POST",
+			data	:	{getProduct:1},
+			success	:	function(data){
+				$("#get_product").html(data);
+			}
+		})
+	}
+    gethomeproduts();
+    function gethomeproduts(){
+		$.ajax({
+			url	:	"homeaction.php",
+			method:	"POST",
+			data	:	{gethomeProduct:1},
+			success	:	function(data){
+				$("#get_home_product").html(data);
+			}
+		})
+	}
+    function producthome(){
+		$.ajax({
+			url	:	"homeaction.php",
+			method:	"POST",
+			data	:	{getProducthome:1},
+			success	:	function(data){
+				$("#get_product_home").html(data);
+			}
+		})
+	}
+   
+    
+	/*	when page is load successfully then there is a list of categories when user click on category we will get category id and 
+		according to id we will show products
+	*/
+	$("body").delegate(".category","click",function(event){
+		$("#get_product").html("<h3>Loading...</h3>");
+		event.preventDefault();
+		var cid = $(this).attr('cid');
+		
+			$.ajax({
+			url		:	"action.php",
+			method	:	"POST",
+			data	:	{get_seleted_Category:1,cat_id:cid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
+    $("body").delegate(".categoryhome","click",function(event){
+		$("#get_product").html("<h3>Loading...</h3>");
+		event.preventDefault();
+		var cid = $(this).attr('cid');
+		
+			$.ajax({
+			url		:	"homeaction.php",
+			method	:	"POST",
+			data	:	{get_seleted_Category:1,cat_id:cid},
+			success	:	function(data){
+				$("#get_product").html(data);
+				if($("body").width() < 480){
+					$("body").scrollTop(683);
+				}
+			}
+		})
+	
+	})
